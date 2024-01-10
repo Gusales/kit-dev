4517e50239c4efe26066c7b7764953cde56c6a0a
= <=>
2024-01-10 15:49:10 -0300

    style and refactor: apply eslint configs and change the height of body

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { ptBr, enUs } from "@/languages/languages.json";
+import { ptBr, enUs } from '@/languages/languages.json'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
@@ -12,9 +12,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: {
     default: 'Kit Dev',
-    template: '%s | Kit Dev'
+    template: '%s | Kit Dev',
   },
-  description: 'Seu Kit de ferramentas para melhorar sua produtividade na programação.',
+  description:
+    'Seu Kit de ferramentas para melhorar sua produtividade na programação.',
 }
 
 export default function RootLayout({
@@ -24,8 +25,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={`${inter.className} bg-zinc-950 h-screen`}>
-        <Header langueBR={ptBr} langueEn={enUs}/>
+      <body className={`${inter.className} bg-zinc-950 min-h-full`}>
+        <Header langueBR={ptBr} langueEn={enUs} />
         {children}
         <Footer />
       </body>
