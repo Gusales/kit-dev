64a2e0b0998a9b0431833d67956cfa437b727c25
G3rsonBR <gersons.br13@gmail.com>
2024-01-10 04:39:29 -0300

    style: Header is now responsive

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Header } from '@/components/Header/index'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={`${inter.className} bg-zinc-950 h-screen`}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,33 +1,6 @@
-import Image from 'next/image'
-import Logo from "@/assets/IconKitDev.svg"
-import { FaUserCircle } from "react-icons/fa";
-import { Switch } from "@/components/ui/switch"
-
-
 export default function Home() {
   return (
-    <div className="bg-zinc-950 h-screen">
-      <header className="h-24 flex justify-around items-center font-semibold text-zinc-100">
-        <div className='flex'>
-          <Image src={Logo} alt='Logo from Kit Dev' width={30}/>
-          <h2 className='text-4xl ml-2'>KIT DEV</h2>
-        </div>
-        <div className='flex w-1/3 justify-evenly items-center'>
-          <label className='flex gap-2'>
-            Br
-            <Switch />
-            En
-          </label>
-          <div className='flex gap-3'>
-            <h3>About</h3>
-            <h3>Projects</h3>
-            <h3>Contact</h3>
-          </div>
-          <div className='flex items-center'>
-            <FaUserCircle size={'2rem'}/>
-          </div>
-        </div>
-      </header>
+    <div>
     </div>
   )
 }
