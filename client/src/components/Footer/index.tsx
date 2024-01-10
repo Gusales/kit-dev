b28cddac024b772c6cc18490c062cdb04c9b7f96
= <=>
2024-01-10 15:48:05 -0300

    refactor: updated the responsive footer's

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 left-0 w-full list-item text-center items-center sm:flex justify-between py-2 px-3 sm:py-7 sm:px-11">
+    <footer className="sm:absolute relative bottom-0 left-0 w-full list-item text-center items-center sm:flex justify-between py-2 px-3 sm:py-7 sm:px-11 sm:text-left">
       <div>
         <p className="text-sm text-white">
           Gostou do site? Te ajudou quando precisou?
@@ -12,10 +12,10 @@ export function Footer() {
           Que tal ajudar a gente a aprimorar cada vez mais?
         </p>
       </div>
-      <div className="flex justify-center p-2">
+      <div className="flex justify-center items-center p-2">
         <a
           href="#"
-          className="text-sm text-white pr-3 hover:underline hover:decoration-white"
+          className="text-sm text-white pr-3 underline hover:text-zinc-500"
         >
           Doe para o café dos Devs!
         </a>
